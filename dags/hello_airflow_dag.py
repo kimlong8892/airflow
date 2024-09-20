57df7058b9d07f290b97696b82b645df56abbd7a
Long Nguyen Kim <longshare9201@gmail.com>
2024-09-20 23:39:12 +0700

    init dag

---
+++ dags/hello_airflow_dag.py
@@ -0,0 +1,33 @@
+from airflow import DAG
+from airflow.operators.python import PythonOperator
+from datetime import datetime, timedelta
+
+# Hàm Python sẽ được gọi khi DAG chạy
+def hello_airflow():
+    print("Hello Airflow!")
+
+# Định nghĩa các tham số cơ bản của DAG
+default_args = {
+    'owner': 'airflow',
+    'start_date': datetime(2023, 9, 20),
+    'retries': 1,
+    'retry_delay': timedelta(minutes=5),
+}
+
+# Khởi tạo DAG
+with DAG(
+    dag_id='hello_airflow_dag',
+    default_args=default_args,
+    schedule_interval='*/1 * * * *',  # Lịch chạy mỗi 1 phút
+    catchup=False,  # Không chạy lại các ngày trước đó
+    tags=['example'],
+) as dag:
+
+    # Định nghĩa task sử dụng PythonOperator
+    hello_task = PythonOperator(
+        task_id='hello_task',
+        python_callable=hello_airflow,  # Hàm Python sẽ được gọi khi task chạy
+    )
+
+    # Cài đặt thứ tự task
+    hello_task
